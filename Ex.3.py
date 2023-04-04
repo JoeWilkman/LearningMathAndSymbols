26a29c4d17fe14522969fb55ee25e03afd0f0072
JoeWilkman <127060760+JoeWilkman@users.noreply.github.com>
2023-04-03 21:28:04 -0400

    Add files via upload

---
+++ Ex.3.py
@@ -0,0 +1,53 @@
+# + = plus (addition)
+# - = minus (subtraction)
+# / = slash (divide)
+# * = asterisk (multiply)
+# % = percent (It returns the remainder of dividing the left hand operand by right hand operand. It's used to get the remainder of a division problem)
+# < = less-than (comapres numbers)
+# > = greater-than (comapres numbers)
+# <= = less-than-equal (comapres numbers)
+# >= = greater-than-equal (comapres numbers)
+
+
+
+#This will print the statement "I will now count my chickens"
+print("I will now count my chickens: ")
+
+#This will print "Hens:" along with the floating integer at the end of teh equation
+print("Hens", float(25 + 30/6))
+
+#This will print "Roosters" along with the floating integer at the end of te equation
+print("Roosters", float(100 - 25 * 3 % 4))
+
+#This will print the statement "Now I will count the eggs:"
+print("Now I will count the eggs:")
+
+#This will print the math equation result as a floating integer
+print(float(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6))
+
+#This will print out the statement "Is it true that 3 + 2 < 5 - 7?"
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#This will calculate the two sides of the equation and compare them only to print out the boolean False as the statement is false
+print(3 + 2 < 5 - 7)
+
+#This will print the statement "What is 3 + 2?" and then calculate the answer to the equation right after it
+print("What is 3 + 2?", float(3 + 2))
+
+#This will print the statement "What is 5 - 7?" and the calculate the answer to the equation right after it
+print("What is 5 - 7", float(5 - 7))
+
+#This will print the statement "Oh, that's why it's False."
+print("Oh, that's why it's False.")
+
+#This will print the statement "How about some more."
+print("How about some more.")
+
+#This will print the statement "Is it greater?" and then the boolean True as the comparrison is True
+print("Is it greater?", 5 > -2)
+
+#This will print the statement "Is it greater or equal?" and then print the boolean True as the comparrison is True
+print("Is it greater or equal?", 5 >= -2)
+
+#This will print the statement "Is it less or equal?" and then the boolean False as the comparrison is False
+print("Is it less or equal?", 5 <= -2)
